431e1ca48c9e592a0e6efa486118fc1f42444b5b
FOSTER_BRAND <155284643+Fosterbrand@users.noreply.github.com>
2025-03-26 06:43:23 +0500

    Update Foster.py

--- Foster.py
+++ Foster.py
@@ -1,4 +1,19 @@
-from Foster import main  
+import sys
+from importlib.util import spec_from_file_location, module_from_spec
+
+try:
+    from Foster import main
+except ImportError:
+    try: 
+        spec = spec_from_file_location("Foster", "Foster.cython-312.so")
+        Foster = module_from_spec(spec)
+        spec.loader.exec_module(Foster)
+        main = Foster.main
+    except Exception as e:
+        print(f"Error loading module: {e}\n"
+              "1. Ensure 'Foster.cython-312.so' exists\n"
+              "2. Run from the same directory")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
